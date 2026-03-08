ec7622c42ebd1614486fc267b7873cb57317678b
Altamash Alam <altamashalam102@gmail.com>
2026-03-09 04:26:08 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const CONFIG = {
-  GITHUB_PROFILE: "https://github.com/altamashalam102",
-  PORTFOLIO_REPO: "https://github.com/altamashalam102/portfolio",
-  LINKEDIN_URL: "https://linkedin.com/in/altamashalam102",
+  GITHUB_PROFILE: "https://github.com/altamashGit",
+  PORTFOLIO_REPO: "https://altamashgit.github.io/altamash-portfolio/",
+  LINKEDIN_URL: "https://www.linkedin.com/in/altamash-alam-129969289/",
   EMAIL: "altamashalam102@gmail.com",
-  RESUME_PATH: "/resume.pdf",
+  RESUME_PATH: "https://docs.google.com/document/d/1uKoZ42miMOcogABn8mkT2Q3i6Cy5aH5Wp-Ba18f5VKI/edit?usp=drive_link",
   PROJECT_UPDATES_PATH: "/project-updates/",
   PROJECT_UPDATES_PREFIX: "project", // Files named project1.txt, project2.txt, etc.
   CERT_UPDATES_PATH: "/certification-updates/",
