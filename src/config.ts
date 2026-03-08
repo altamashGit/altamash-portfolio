336e7115788154442eb4aa25f52b29c7bc587cf0
Altamash Alam <altamashalam102@gmail.com>
2026-03-09 04:40:31 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   GITHUB_PROFILE: "https://github.com/altamashGit",
-  PORTFOLIO_REPO: "https://altamashgit.github.io/altamash-portfolio/",
+  PORTFOLIO_REPO: "https://github.com/altamashGit/altamsh-portfolio.git",
   LINKEDIN_URL: "https://www.linkedin.com/in/altamash-alam-129969289/",
   EMAIL: "altamashalam102@gmail.com",
   RESUME_PATH: "https://docs.google.com/document/d/1uKoZ42miMOcogABn8mkT2Q3i6Cy5aH5Wp-Ba18f5VKI/edit?usp=sharing",
