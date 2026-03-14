204b95a029a06370fdb00089e2fb1b5ac4cddf21
Altamash Alam <altamashalam102@gmail.com>
2026-03-15 00:26:42 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const CONFIG = {
   LINKEDIN_URL: "https://www.linkedin.com/in/altamash-alam-129969289/",
   EMAIL: "altamashalam102@gmail.com",
   RESUME_PATH: "https://docs.google.com/document/d/1uKoZ42miMOcogABn8mkT2Q3i6Cy5aH5Wp-Ba18f5VKI/edit?usp=sharing",
-  PROJECT_UPDATES_PATH: "/project-updates/",
+  PROJECT_UPDATES_PATH: "project-updates/",
   PROJECT_UPDATES_PREFIX: "project", // Files named project1.txt, project2.txt, etc.
   CERT_UPDATES_PATH: "/certification-updates/",
   CERT_UPDATES_PREFIX: "certification", // Files named certification1.txt, certification2.txt, etc.
