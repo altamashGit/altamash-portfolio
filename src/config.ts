33f0b2b235d972911d24356539d52f58a2f30df0
Altamash Alam <altamashalam102@gmail.com>
2026-03-15 00:00:03 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const CONFIG = {
   EMAIL: "altamashalam102@gmail.com",
   RESUME_PATH: "https://docs.google.com/document/d/1uKoZ42miMOcogABn8mkT2Q3i6Cy5aH5Wp-Ba18f5VKI/edit?usp=sharing",
   PROJECT_UPDATES_PATH: "/project-updates/",
-  PROJECT_UPDATES_PREFIX: "project", // Files named project1.txt, project2.txt, etc.
+  PROJECT_UPDATES_PREFIX: "project","project4.txt" // Files named project1.txt, project2.txt, etc.
   CERT_UPDATES_PATH: "/certification-updates/",
   CERT_UPDATES_PREFIX: "certification", // Files named certification1.txt, certification2.txt, etc.
   MAX_UPDATES: 50 // Safety limit for sequential fetching
